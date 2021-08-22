268d970cae1207d32a9fde04e7861b9caeea276c
Crystal Taylor <kittytaylor@gmail.com>
2021-08-21 21:30:55 -0700

    Got heroku working

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,50 +1,62 @@
 // LOAD DATA
-const path = require('path');
-const notesDB = require('../db/db.json');
-const fs = require('fs')
-var dataBase = []
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const notesDB = require("../db/db.json");
+const fs = require("fs");
+var dataBase = [];
 
 // ROUTING
 module.exports = (app) => {
-     // API GET Requests
-     app.get('/api/notes', (req, res) => {
-        const allNotes = fs.readFileSync((path.join(__dirname, './db/db.json')), 'utf8');
-  
-        res.send(allNotes);
-      });
+  // API GET Requests
+  app.get("/api/notes", (req, res) => {
+    const allNotes = fs.readFileSync("./db/db.json", "utf8");
+    res.send(allNotes);
+  });
 
-
-    app.post('/api/notes', (req,res) => {
-        let newNote = req.body;
+  app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
     newNote.id = createId();
 
-    let notesArr = JSON.parse(fs.readFileSync((path.join(__dirname, '../db/db.json')), 'utf8'));
+    let notesArr = JSON.parse(
+      fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")
+    );
     notesArr.push(newNote);
 
-    fs.writeFile((path.join(__dirname, '../db/db.json')), JSON.stringify(notesArr, null, 2), (err) => {err? console.error(err) : 
-        console.log("Successfully wrote db.json")});
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notesArr, null, 2),
+      (err) => {
+        err ? console.error(err) : console.log("Successfully wrote db.json");
+      }
+    );
     res.json(newNote);
-
   });
+
   function createId() {
+    //creating a unique user ID
     const uniqueId = uuidv4();
     return uniqueId;
   }
 
+  // Empty out the arrays of data
+  app.delete("/api/notes/:id", (req, res) => {
+    const deleteID = req.params.id;
+    let notesArr = JSON.parse(
+      fs.readFileSync(path.join("../db/db.json"), "utf8")
+    );
 
-}
+    let deleteNote = notesArr.find((note) => note.id === deleteID);
+    let position = notesArr.indexOf(deleteNote);
+    notesArr.splice(position, 1);
 
-    // Empty out the arrays of data
-    app.delete('/api/notes/:id', (req, res) => {
-        const deleteID = req.params.id;
-        let notesArr = JSON.parse(fs.readFileSync((path.join(__dirname, '../db/db.json')), 'utf8'));
-    
-        let deleteNote = notesArr.find((note) => note.id === deleteID);
-        let position = notesArr.indexOf(deleteNote);
-        notesArr.splice(position, 1);
-    
-        fs.writeFile((path.join(__dirname, '../db/db.json')), JSON.stringify(notesArr, null, 2), (err) => {err? console.error(err) : 
-            console.log("Successfully wrote db.json")});
-    
-        res.json({ ok: true });
-      });
+    fs.writeFile(
+      path.join("../db/db.json"),
+      JSON.stringify(notesArr, null, 2),
+      (err) => {
+        err ? console.error(err) : console.log("Successfully wrote db.json");
+      }
+    );
+
+    res.json({ ok: true });
+  });
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Note-Taker
 Express.js: Note Taker
+
+Heroku link: https://pure-mesa-69970.herokuapp.com/notes
